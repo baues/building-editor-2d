f8406162e845c225c6c2dddf229756c8dd030399
hrntsm <contact@hrntsm.com>
2022-12-25 22:49:17 +0900

    Update readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
 
 export default function Home() {
   if (typeof window !== 'undefined') {
