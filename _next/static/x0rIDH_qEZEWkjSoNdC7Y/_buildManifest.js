209ae64b5520a79f688c5ad776b72afcbf126ad9
hrntsm <hrntsm@users.noreply.github.com>
2022-12-27 05:35:08 +0000

    deploy: 72904466b4cdd4a123c588ae76a3c055a3e0e45e

---
+++ _next/static/x0rIDH_qEZEWkjSoNdC7Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6584ebf09c3b4dd3.js"],"/_error":["static/chunks/pages/_error-753686b3518a09c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
