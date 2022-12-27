5d2da0880e3da96f1cb4784b446af68af634e843
hrntsm <contact@hrntsm.com>
2022-12-27 14:11:18 +0900

    Add head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 const Editor = dynamic(() => import('../editor-ui'), { ssr: false });
 
 export default function Home() {
-  return <Editor />;
+  return (
+    <div>
+      <Head>
+        <title>BAUES Building Editor 2D</title>
+        <meta name="description" content="BAUES Building Editor 2D" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Editor />
+    </div>
+  );
 }
